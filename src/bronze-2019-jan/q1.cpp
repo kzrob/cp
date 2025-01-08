c3299edd8bdd98aef7ee60c60975bade12a703f5
Kevin <davidyo571@gmail.com>
2025-01-08 11:23:58 -0500

    save

---
+++ src/bronze-2019-jan/q1.cpp
@@ -0,0 +1,18 @@
+﻿#include <bits/stdc++.h>
+using namespace std;
+
+void swap(int cups[3], int a, int b, int g) {
+    int temp = cups[a];
+    cups[a] = cups[b];
+    cups[b] = temp;
+}
+
+int main() {
+    //fast io
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    
+    cout << "awd";
+    
+    return 0;
+}
