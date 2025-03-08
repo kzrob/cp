274c5bad0366b067820297e2c92e0e04aca43fb1
Kevin <davidyo571@gmail.com>
2025-03-08 12:32:13 -0500

    made q3.cpp prettier

--- bronze/2025-01/q3.cpp
+++ bronze/2025-01/q3.cpp
@@ -6,59 +6,41 @@ int main() {
 	ios::sync_with_stdio(false);
 	cin.tie(nullptr);
 
-	//set the arrays
+	//inputs
 	int N;
 	cin >> N;
-	int orig[N];
-	for (int i=0; i<N; ++i) {
-		int v;
-		cin >> v;
-		orig[i] = v;
-	}
-	int check[N];
-	for (int i=0; i<N; ++i) {
-		int v;
-		cin >> v;
-		check[i] = v;
+
+	vector<int> orig(N);
+	for (int &i : orig) {
+		cin >> i;
 	}
 
-	int output[N+1] = {0};
+	vector<int> check(N);
+	for (int &i : check) {
+		cin >> i;
+	}
+
+	vector<int> output(N+1, 0);
 
 	//lambada
-	auto checkArr = [&](int* arr) {
+	auto checkDiff = [&](vector<int> arr) {
 		int count = 0;
-		for (int i=0; i<N; ++i) {
+		for (int i=0; i<N; i++) {
 			if (arr[i] == check[i]) {
-				++count;
+				count++;
 			}
 		}
-		return count;
+		output[count]++;
 	};
 
 	//logic
-	for (int l=0; l<N; ++l) {
-		for (int r=l; r<N; ++r) {
-			int t[N];
-			copy(orig, orig + N, t);
-
-			//reverse
-			int a = l;
-			int b = r;
-
-			while (a < b) {
-				int temp = t[a];
-				t[a] = t[b];
-				t[b] = temp;
-
-				++a;
-				--b;
-			}
-			
-			int count = checkArr(t);
-			output[count]++;
+	for (int l=0; l<N; l++) {
+		for (int r=l; r<N; r++) {
+			vector<int> test = orig;
+            reverse(test.begin() + l, test.begin() + r+1);
+			checkDiff(test);
 		}
 	}
-
 
 	//output
 	for (int v : output) {
