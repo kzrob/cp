cc202d7f1cf2789b83c8f2fd3780b9028f4ec355
kzrob <davidyo571@gmail.com>
2025-11-05 18:25:41 -0500

    refactor (messing around)

--- usaco/bronze/2025-02/1.cpp
+++ usaco/bronze/2025-02/1.cpp
@@ -1,6 +1,30 @@
 #include <bits/stdc++.h>
 using namespace std;
 
+int countMin(const vector<vector<bool>> &grid, int x, int y) {
+    int N = grid.size()-1;
+    int hash = 0;
+    int dot = 0;
+
+    //q1
+    if (grid[y][x])
+    { hash++; } else { dot++; }
+
+    //q2
+    if (grid[y][N-x])
+    { hash++; } else { dot++; }
+        
+    //q3
+    if (grid[N-y][N-x])
+    { hash++; } else { dot++; }
+
+    //q4
+    if (grid[N-y][x])
+    { hash++; } else { dot++; }
+
+    return min(hash, dot);
+}
+
 int main() {
     //fast io
     ios::sync_with_stdio(false);
@@ -13,43 +37,19 @@ int main() {
     //input the grid
     //true = #, false = .
     vector<vector<bool>> grid(N, vector<bool>(N, 0));
-    for (int y=0; y<N; y++) {
-        for (int x=0; x<N; x++) {
+    for (int y = 0; y < N; y++) {
+        for (int x = 0; x < N; x++) {
             char c;
             cin >> c;
             grid[y][x] = (c == '#');
         }
     }
 
-    //lambada function to count hashtags and dots
-    auto countMin = [&](int x, int y) {
-        int hash = 0;
-        int dot = 0;
-
-        //q1
-        if (grid[y][x])
-        { hash++; } else { dot++; }
-
-        //q2
-        if (grid[y][N-1-x])
-        { hash++; } else { dot++; }
-            
-        //q3
-        if (grid[N-1-y][N-1-x])
-        { hash++; } else { dot++; }
-
-        //q4
-        if (grid[N-1-y][x])
-        { hash++; } else { dot++; }
-
-        return min(hash, dot);
-    };
-
     //initial changes
     int changes = 0;
     for (int y = 0; y < N/2; y++) {
         for (int x = N/2; x < N; x++) {
-            changes += countMin(x, y);
+            changes += countMin(grid, x, y);
         }
     }
     cout << changes << "\n";
@@ -60,12 +60,12 @@ int main() {
         cin >> y >> x;
         y--; x--; //remove the one index
 
-        //old counts
-        changes -= countMin(x, y);
+        //old counts - correcting for overcounting
+        changes -= countMin(grid, x, y);
 
-        //new counts
+        //new counts - after the change
         grid[y][x] = !grid[y][x];
-        changes += countMin(x, y);
+        changes += countMin(grid, x, y);
         
         cout << changes << "\n";
     }
